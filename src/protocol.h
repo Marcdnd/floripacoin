03b93239e5dda6bd7d9c2823404b3908ad969c5e
root <root@floripacoin.(none)>
2014-03-17 14:45:21 -0400

    Seed ports

--- src/protocol.h
+++ src/protocol.h
@@ -18,7 +18,7 @@
 extern bool fTestNet;
 static inline unsigned short GetDefaultPort(const bool testnet = fTestNet)
 {
-    return testnet ? 11333 : 1333;
+    return testnet ? 18124 : 18124;
 }
 
 
